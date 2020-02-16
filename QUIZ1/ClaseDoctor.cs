3335c535155200641106130dd008632013be7307
JeffersonLaptop <jefferson.gonzalez@uamcr.net>
2020-02-16 15:57:47 -0600

    QUIZ1 ClasePersona, ClaseDoctor

---
+++ QUIZ1/ClaseDoctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ1
+{
+    class ClaseDoctor : ClasePersona
+    {
+        //Atributos
+
+        private int _especialidad;
+
+        // get / set
+        public int Especialidad { get => _especialidad; set => _especialidad = value; }
+
+        // contructores
+        public ClaseDoctor()
+        {
+        }
+
+        public ClaseDoctor(int especialidad)
+        {
+            _especialidad = especialidad;
+        }
+
+        // Contructor que llama la clase base (ClasePersona)
+        public ClaseDoctor(int especialidad, int id, string nombre, string apellido, int cedula) 
+            : base( id, nombre, apellido, cedula)
+        {
+            _especialidad = especialidad;
+        }
+
+        // override ToString
+
+        public override String NombreCompleto()
+        {
+            Console.WriteLine(Nombre);
+            return string.Format(" Id: {0}, /n Nombre: {1},/n Especialidad: {2} ", Id, Nombre, Especialidad);
+        }
+
+    }
+}
---
+++ QUIZ1/ClasePersona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ1
+{
+    class ClasePersona
+    {
+
+        #region AtributosPersona
+
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private int _cedula;
+
+
+        // set / get
+        public int Id { get => _id; set => _id = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public int Cedula { get => _cedula; set => _cedula = value; }
+
+
+        //Constructores
+        public ClasePersona()
+        {
+        }
+
+        public ClasePersona(int id, string nombre, string apellido, int cedula)
+        {
+            _id = id;
+            _nombre = nombre;
+            _apellido = apellido;
+            _cedula = cedula;
+        }
+
+        #endregion
+
+        public virtual String  NombreCompleto()
+        {
+
+            return string.Format("Nombre: {0}, {1}", _apellido, _nombre);
+        }
+
+    }
+}
